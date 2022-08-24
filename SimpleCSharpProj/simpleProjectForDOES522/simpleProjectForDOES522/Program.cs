bbcb5f22261b4a1cf3f1b1d0a859a1d5a8cdaf1d
Daniel Russouw <dan7493@hotmail.com>
2022-08-24 19:07:39 +0200

    Added an additional WriteLine statement

--- SimpleCSharpProj/simpleProjectForDOES522/simpleProjectForDOES522/Program.cs
+++ SimpleCSharpProj/simpleProjectForDOES522/simpleProjectForDOES522/Program.cs
@@ -9,6 +9,7 @@ namespace simpleProjectForDOES522
             Console.WriteLine("Hello, what is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Hello {0}, I am a simple program!", name);
+            Console.WriteLine("But before I go, this is my second revision of this project");
         }
     }
 }
