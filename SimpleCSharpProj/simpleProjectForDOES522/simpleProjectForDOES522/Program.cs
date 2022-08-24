386630fb11acd89659078284c52aca06e99ca11c
Daniel Russouw <dan7493@hotmail.com>
2022-08-24 18:48:27 +0200

    Initial Commit

---
+++ SimpleCSharpProj/simpleProjectForDOES522/simpleProjectForDOES522/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace simpleProjectForDOES522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello {0}, I am a simple program!", name);
+        }
+    }
+}
